f76a31eccecbcf9be147d0f853869399c847113b
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2022-09-14 20:04:39 +0430

    fix eslint errors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Home = () => (
   <section id="home">
-    <h1>Welcome to our litle app Math lover!</h1>
+    <h1>Welcome to our page</h1>
     <p className="homeContent">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in ligula tortor. Aliquam fermentum enim ac turpis congue, sed accumsan lorem mollis. Pellentesque ultricies, sapien at porta tincidunt, erat turpis posuere ante, nec viverra tellus dolor nec velit. Aenean fringilla luctus purus, et interdum est rhoncus nec. Suspendisse eu dui a turpis dignissim aliquet. Aenean viverra ultrices lacinia. Fusce tristique nulla lectus, non bibendum risus commodo eget. Aliquam aliquam ipsum eget nisi lobortis porta. Donec ornare tortor elementum ex blandit pharetra.</p>
     <p className="homeContent">
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae labore, tempora cupiditate tenetur ducimus dolore maxime voluptas ea itaque facere unde distinctio, inventore ullam, facilis quas quis et. Natus, repellendus.
