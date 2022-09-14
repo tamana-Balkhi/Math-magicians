7db7768e8123f1da3ab59d0f4feabc7987ee19cb
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2022-09-14 17:53:54 +0430

    create quets page

---
+++ src/pages/Quotes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Quote = () => (
+  <section id="quote">
+    <p>
+      &quot;God used beautiful mathematics in creating the world.&quot;
+    </p>
+    <span>Pual Direc</span>
+  </section>
+);
+export default Quote;
